23f9d68137f66d419d8dfca770233d4b118e8155
Drake Damon <dddamon06@gmail.com>
2025-02-16 17:57:08 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
+from sqlalchemy import desc  # Add this line
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
@@ -20,19 +20,43 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+
+    response = make_response(
+        jsonify([bakery.to_dict() for bakery in bakeries]),
+        200
+    )
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakeries = Bakery.query.filter_by(id=id).first()
+
+    response = make_response(
+        jsonify(bakeries.to_dict()),
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    response = make_response(
+        jsonify([bg.to_dict() for bg in baked_goods]),
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
-
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    
+    if most_expensive:
+        return jsonify(most_expensive.to_dict()), 200, {'Content-Type': 'application/json'}
+    else:
+        return jsonify({"error": "No baked goods found"}), 404, {'Content-Type': 'application/json'}
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
